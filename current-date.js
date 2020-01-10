e2588bd80ef88e2c31e0edbb2a1c8dff930a0966
Grady Hodge <gradywhodge@gmail.com>
2020-01-10 10:37:22 -0600

    changed dynamic dates to reflect lastModified

--- current-date.js
+++ current-date.js
@@ -1,6 +1,6 @@
 const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('fullDate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('fullDate').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
 
-document.getElementById('time').textContent = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+document.getElementById('time').textContent = new Date(document.lastModified).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
 
 document.getElementById('updateYear').textContent = new Date().getFullYear();
