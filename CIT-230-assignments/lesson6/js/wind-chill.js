2381ec64de476678eab8a0f342b06f15df69cbcb
Grady Hodge <gradywhodge@gmail.com>
2020-02-19 11:38:57 -0600

    refactoring windchill to be dynamic with API info

--- CIT-230-assignments/lesson6/js/wind-chill.js
+++ CIT-230-assignments/lesson6/js/wind-chill.js
@@ -1,32 +1,26 @@
-<script language="javascript" type="text/javascript">
+function doInputOutput() {
+  //Can I remove the var from the input?
+  var temp = parseFloat(document.getElementById("temp").value);
+  var windSpeed = parseFloat(document.getElementById("windSpeed").value);
+  //windChill();
+  var itsCold = windChill(temp, windSpeed);
 
-function doInputOutput () {
-//Can I remove the var from the input?
-var temp = parseFloat (document.getElementById('temp').value);
-var windSpeed = parseFloat (document.getElementById('windSpeed').value);
-//windChill();
-var itsCold = windChill(temp,windSpeed)
-
-var div = document.getElementById ('output');
-	div.innerHTML = itsCold;
+  var div = document.getElementById("output");
+  div.innerHTML = itsCold;
 }
 
-function windChill (t,v) {
+function windChill(t, v) {
+  var itFeelsLike = 35.74 + 0.6215 * t - 35.75 * a + 0.4275 * t * a;
+  var a = Math.pow(v, 0.16);
 
-var itFeelsLike = 35.74 + 0.6215 * t -35.75* a + .4275 * t * a;
-var a = Math.pow(v,.16);
+  //I got the formula from www.onlineconversion.com/windchill.htm.
+  //I verified the formula from a few separate sources as well
 
-
-//I got the formula from www.onlineconversion.com/windchill.htm.  
-//I verified the formula from a few separate sources as well
-
-//var itFeelsLike =___________
-return itFeelslike;
-
+  //var itFeelsLike =___________
+  return itFeelslike;
 }
 
-
-</script>
+/* 
 
 <body>
 
@@ -38,4 +32,4 @@ return itFeelslike;
 <!--<button type="button" onclick="fifteen();">Click</button>-->
 <div id="output"></div>
 </body>
-</html>
+</html> */
