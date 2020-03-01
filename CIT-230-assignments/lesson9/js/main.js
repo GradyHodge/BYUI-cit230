f6a09c92a3d968c07d6e714973c9613e5f47198e
Grady Hodge <gradywhodge@gmail.com>
2020-02-29 20:33:15 -0600

    lesson 8 tables and forms

---
+++ CIT-230-assignments/lesson9/js/main.js
@@ -0,0 +1,97 @@
+// LESSON 7 PRESTON TOWN CENTER
+
+// TODO: POP-UP ALERT BASED ON DATE
+
+var date = new Date();
+var day = date.getDay();
+if (day != 5) {
+  document.getElementById("popup").style.display = "none";
+}
+
+//////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////
+
+// TODO: weather API
+
+///////////////////////////////////////////////////////////////////////////
+
+// FOOTER: Todays Current Date Formatted as Day, Date Month, Year
+
+//DAY Full Name Array
+const dayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+// Month Full Name Array
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+const todaysDate = new Date();
+const dayName = dayNames[todaysDate.getDay()];
+const monthName = months[todaysDate.getMonth()];
+const today =
+  dayName +
+  ", " +
+  todaysDate.getDate() +
+  " " +
+  monthName +
+  ", " +
+  todaysDate.getFullYear();
+
+document.getElementById("today").textContent = today;
+
+///////////////////////////////////////////////////////////////////////////
+
+const todaysDate1 = new Date();
+const dayName1 = dayNames[todaysDate.getDay()];
+const monthName1 = months[todaysDate.getMonth()];
+const today1 = dayName + ", " + todaysDate.getDate() + " " + monthName;
+
+document.getElementById("todaySummary").textContent = today1;
+
+///////////////////////////////////////////////////////////////////////////
+
+//FOOTER: Last Modified date
+
+const options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+  year: "numeric"
+};
+document.getElementById("fullDate").textContent = new Date(
+  document.lastModified
+).toLocaleDateString("en-US", options);
+
+///////////////////////////////////////////////////////////////////////////
+
+//footer update Last Modified time stamp
+
+document.getElementById("time").textContent = new Date(
+  document.lastModified
+).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+///////////////////////////////////////////////////////////////////////////
+
+//footer update copyright year
+
+document.getElementById("updateYear").textContent = new Date().getFullYear();
