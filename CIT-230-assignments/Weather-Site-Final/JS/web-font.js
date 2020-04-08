dcb1da59454e4aaf33cd674d9718b34093f1920d
Grady Hodge <gradywhodge@gmail.com>
2020-04-08 03:41:54 -0500

    set up folder structure for Salmon River Adventures website including SCSS

---
+++ CIT-230-assignments/Weather-Site-Final/JS/web-font.js
@@ -0,0 +1,7 @@
+src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+
+WebFont.load({
+  google: {
+    families: ["Alegreya SC", "Alegreya", "Alegreya Sans", "Lora"],
+  },
+});
