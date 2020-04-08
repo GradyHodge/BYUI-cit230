4381bcde780074a73fd85e51299b68e2cebc9322
Grady Hodge <gradywhodge@gmail.com>
2020-04-08 13:46:04 -0500

    fixed responsive navbar

--- CIT-230-assignments/Weather-Site-Final/JS/toggle.js
+++ CIT-230-assignments/Weather-Site-Final/JS/toggle.js
@@ -10,4 +10,9 @@ function toggleMenu() {
   document.getElementById("navUl").classList.toggle("hide");
 }
 
-///////////////////////////////////////////////////////////////////////////
+// ///////////////////////////////////////////////////////////////////////////
+
+// const hambutton = document.querySelector('.ham');
+// const mainnav = document.querySelector('.navigation')
+
+// hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
