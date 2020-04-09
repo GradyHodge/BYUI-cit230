dfaf2da8afa1c5686d6eb165b4ac021eebace061
Grady Hodge <gradywhodge@gmail.com>
2020-04-09 13:06:13 -0500

    last minute stuff

--- CIT-230-assignments/Weather-Site-Final/JS/guides.js
+++ CIT-230-assignments/Weather-Site-Final/JS/guides.js
@@ -2,32 +2,32 @@
 
 //trying to get anything to work as a starting point
 ///////////////////////////////////////////////////////////////////////////////
-$(document).ready(function () {
-  $.getJSON("./guides.json", function (result) {
-    var guidesObj = result;
+// $(document).ready(function () {
+//   $.getJSON("./guides.json", function (result) {
+//     var guidesObj = result;
 
-    guidesObj.guides.forEach((guide) => {
-      var name = $("<h4></h4>").text(guides.name);
-      var picture = $("<img alt='mug shot'/><br>").attr("src", guides.picture);
-      var certification = $("<br><span></span><br>").text(
-        "Certification: " + guides.cert
-      );
-      var experience = $("<br/><span></span>").text(
-        "Experience: " + guides.exp
-      );
-      var email = $("<br/><span></span>").text("Email: " + guides.email);
-      var biography = $("<br/><span></span><br/><br/>").text(
-        "Bio: " + guides.bio
-      );
-      $(".container").append(
-        name,
-        picture,
-        certification,
-        experience,
-        email,
-        biography
-      );
-    });
-  });
-});
+//     guidesObj.guides.forEach((guide) => {
+//       var name = $("<h4></h4>").text(guides.name);
+//       var picture = $("<img alt='mug shot'/><br>").attr("src", guides.picture);
+//       var certification = $("<br><span></span><br>").text(
+//         "Certification: " + guides.cert
+//       );
+//       var experience = $("<br/><span></span>").text(
+//         "Experience: " + guides.exp
+//       );
+//       var email = $("<br/><span></span>").text("Email: " + guides.email);
+//       var biography = $("<br/><span></span><br/><br/>").text(
+//         "Bio: " + guides.bio
+//       );
+//       $(".container").append(
+//         name,
+//         picture,
+//         certification,
+//         experience,
+//         email,
+//         biography
+//       );
+//     });
+//   });
+// });
 ///////////////////////////////////////////////////////////////////////////
