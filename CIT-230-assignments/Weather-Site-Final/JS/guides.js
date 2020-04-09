6310b05e729f858ea45f2995201b67f1bb4d1dfb
Grady Hodge <gradywhodge@gmail.com>
2020-04-08 22:52:10 -0500

    reworking json using method from lesson 10

---
+++ CIT-230-assignments/Weather-Site-Final/JS/guides.js
@@ -0,0 +1,32 @@
+//JS for Fake Guide JSON info
+
+///////////////////////////////////////////////////////////////////////////////
+$(document).ready(function () {
+  $.getJSON("./guides.json", function (result) {
+    var guidesObj = result;
+
+    guidesObj.guides.forEach((guide) => {
+      var name = $("<h4></h4>").text(guides.name);
+      var picture = $("<img alt='mug shot'/><br>").attr("src", guides.picture);
+      var certification = $("<br><span></span><br>").text(
+        "Certification: " + guides.cert
+      );
+      var experience = $("<br/><span></span>").text(
+        "Experience: " + guides.exp
+      );
+      var email = $("<br/><span></span>").text("Email: " + guides.email);
+      var biography = $("<br/><span></span><br/><br/>").text(
+        "Bio: " + guides.bio
+      );
+      $(".container").append(
+        name,
+        picture,
+        certification,
+        experience,
+        email,
+        biography
+      );
+    });
+  });
+});
+///////////////////////////////////////////////////////////////////////////
